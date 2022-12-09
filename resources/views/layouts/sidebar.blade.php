45c6dcfd9bbda79a3537382ccc85fc6f287b907c
Akrimi1 <elakrimiiheb@gmail.com>
2022-12-09 23:54:29 +0100

    Fix logo to be shown when changed

--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -1,20 +1,40 @@
-<!-- Main Sidebar Container -->
-<aside class="main-sidebar sidebar-{{setting('theme_contrast')}}-{{setting('theme_color')}} cus_shadow">
-    <!-- Brand Logo -->
-    <a href="{{url('dashboard')}}" class="brand-link {{setting('logo_bg_color','bg-white')}}">
-        <img src="{{ url('/') }}/images/logo_default.png" alt="{{setting('app_name')}}" class="brand-image">
-        <!--<img src="{{$app_logo}}" alt="{{setting('app_name')}}" class="brand-image">-->
-        <span class="brand-text font-weight-light"></span>
-    </a>
-    <!-- Sidebar -->
-    <div class="sidebar">
-        <!-- Sidebar Menu -->
-        <nav class="mt-2">
-            <ul class="nav nav-pills nav-sidebar flex-column sidebar-menu" data-widget="treeview" role="menu" data-accordion="false">
-                @include('layouts.menu',['icons'=>true])
-            </ul>
-        </nav>
-        <!-- /.sidebar-menu -->
-    </div>
-    <!-- /.sidebar -->
-</aside>
+<!-- Main Sidebar Container -->
+
+<aside class="main-sidebar sidebar-{{setting('theme_contrast')}}-{{setting('theme_color')}} cus_shadow">
+
+    <!-- Brand Logo -->
+
+    <a href="{{url('dashboard')}}" class="brand-link {{setting('logo_bg_color','bg-white')}}">
+
+        <!--<img src="{{-- {{ url('/') }} --}} /images/logo_default.png" alt="{{setting('app_name')}}" class="brand-image">-->
+
+        <img src="{{$app_logo}}" type="image/png" alt="{{setting('app_name')}}" class="brand-image">
+
+        <span class="brand-text font-weight-light"></span>
+
+    </a>
+
+    <!-- Sidebar -->
+
+    <div class="sidebar">
+
+        <!-- Sidebar Menu -->
+
+        <nav class="mt-2">
+
+            <ul class="nav nav-pills nav-sidebar flex-column sidebar-menu" data-widget="treeview" role="menu" data-accordion="false">
+
+                @include('layouts.menu',['icons'=>true])
+
+            </ul>
+
+        </nav>
+
+        <!-- /.sidebar-menu -->
+
+    </div>
+
+    <!-- /.sidebar -->
+
+</aside>
+
